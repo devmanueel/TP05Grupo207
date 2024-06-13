c954dfa36737bc2375c4575434dd8b4246193953
Manuel Santiago Mendoza <manuumendozaa05@gmail.com>
2024-06-13 19:04:04 -0300

    Inicializacion del Proyecto TP5 GRUPO-207 Mendoza Manuel Santiago

---
+++ src/main/java/ar/edu/unju/fi/Tp05Grupo207Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp05Grupo207Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp05Grupo207Application.class, args);
+	}
+
+}
